4de325c24b1d829a19ccfbdb454d822819c2834f
praneeth-bala <praneethb2002@gmail.com>
2023-10-03 15:31:22 +0800

    minor fix

--- scratch/2p.cc
+++ scratch/2p.cc
@@ -15,7 +15,6 @@
  */
 
 #include "ns3/core-module.h"
-#include "ns3/cosim-simulator-impl.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
